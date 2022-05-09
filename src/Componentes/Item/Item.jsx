a8994a19febfeae2c7265b27aa41d9aa3a5a4a50
Martin Toffolo <martin.toffolo@gmail.com>
2022-05-09 00:23:51 -0300

    detail

--- src/Componentes/Item/Item.jsx
+++ src/Componentes/Item/Item.jsx
@@ -4,6 +4,7 @@ import {
   Button,
 } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Link } from "react-router-dom";
 
 
 function Item({ producto }) {
@@ -13,10 +14,10 @@ function Item({ producto }) {
   <Card.Img variant="top" src={producto.img} alt={producto.name} height='250 px'/>
   <Card.Body className="d-flex flex-column">
     <Card.Title className='flex-grow-1 text-center'>{producto.name}</Card.Title>
-    <Card.Text className="mt-auto text-center">
+    <Card.Text className="mt-auto text-center h5">
     $ {producto.price}
     </Card.Text>
-    <Button variant="outline-dark">Detalle</Button>
+    <Link to={`/Item/${producto.id}`} className='text-center'><Button variant="outline-dark">Detalle</Button></Link>
   </Card.Body>
 </Card>
     </>
--- src/Componentes/ItemDetail/ItemDetail.jsx
+++ src/Componentes/ItemDetail/ItemDetail.jsx
@@ -11,14 +11,14 @@ const PlantDetail = ({ producto }) => {
         <img
           alt='ecommerce'
           className='lg:w-2/5 w-full object-cover object-center rounded border border-gray-200'
-          src={producto.imagen}
+          src={producto.img}
         />
         <div className='lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0'>
           <h2 className='text-sm title-font text-gray-500 tracking-widest'>
-            GARDEN & DECO
+            PASTELERIA TOFFO
           </h2>
           <h1 className='text-gray-900 text-3xl title-font font-medium mb-1'>
-            {producto.nombre}
+            {producto.name}
           </h1>
           <div className='flex mb-4'>
             <span className='flex items-center'>
@@ -118,36 +118,12 @@ const PlantDetail = ({ producto }) => {
               </Link>
             </span>
           </div>
-          <p className='leading-relaxed'>{producto.descripcion}</p>
+          <p className='leading-relaxed'>{producto.description}</p>
           <div className='flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5'>
-            <div className='flex'>
-              <span className='mr-3'>Color</span>
-              <button className='border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none'></button>
-              <button className='border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none'></button>
-              <button className='border-2 border-gray-300 ml-1 bg-red-500 rounded-full w-6 h-6 focus:outline-none'></button>
-            </div>
+          
             <div className='flex ml-6 items-center'>
-              <span className='mr-3'>Size</span>
-              <div className='relative'>
-                <select className='rounded border appearance-none border-gray-400 py-2 focus:outline-none focus:border-red-500 text-base pl-3 pr-10'>
-                  <option>30 cm</option>
-                  <option>45 cm</option>
-                  <option>60 cm</option>
-                </select>
-                <span className='absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center'>
-                  <svg
-                    fill='none'
-                    stroke='currentColor'
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    strokeWidth='2'
-                    className='w-4 h-4'
-                    viewBox='0 0 24 24'
-                  >
-                    <path d='M6 9l6 6 6-6'></path>
-                  </svg>
-                </span>
-              </div>
+          
+             
             </div>
           </div>
           <div className='flex'>
